934360374450e071e1d5dec51f855460378c03f0
Keshav Modi <keshav.modi@bankbazaar.com>
2015-08-29 22:54:01 +0530

    UserController

--- mitra/grails-app/controllers/mitra/UserController.groovy
+++ mitra/grails-app/controllers/mitra/UserController.groovy
@@ -8,14 +8,15 @@ class UserController {
 
     def subscribe = {
         def deviceId = params.DEVICE_ID
-        def tagList = params.TAG_LIST?.split(",")
+        def tagList = params.TAG_LIST?.split(",") ?: []
         def role = Role.findByName(MitraConstants.ANSWERER_ROLE_NAME)
 
         User user = new User(deviceId:deviceId, tags: tagList, roles: [role])
-        user.save()
-
+        user.save(flush: true)
+        println user.properties
         def returnMap = [:]
         returnMap.status = "SUCCESS"
+//        returnMap.user = user
         def jsonResponse = returnMap as JSON
         render(text: jsonResponse, contentType: "application/json", encoding: "UTF-8");
     }
--- mitra/grails-app/domain/mitra/User.groovy
+++ mitra/grails-app/domain/mitra/User.groovy
@@ -5,7 +5,18 @@ class User {
     String deviceId
     Integer id;
     static hasMany = [tags: Tag, roles: Role]
+    Date updatedOn
 
     static constraints = {
+
     }
+
+    def beforeUpdate = {
+        updatedOn = new Date()
+    }
+
+    def beforeInsert = {
+        updatedOn = new Date()
+    }
+
 }
